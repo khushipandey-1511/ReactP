8f99e20890f4750dcc2b3577fc3379763b19720f
Khushi Pandey <khushipandey151105@gmail.com>
2024-05-03 17:41:11 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,25 @@ import Counter from "./Counter";
 import LikeButton from './LikeButton';
 import LudoBoard from './LudoBoard';
 import TodoList from './TodoList';
+
 import Lottery from './Lottery';
-import Ticket from './Ticket';
+import { sum } from './helper';
+
+
 
 
 function App() {
+
+  let winCondition =(ticket)=>{
+ return ticket.every((num)=>num===ticket[0])
+  }
+
+  
   return(
  <>
-
-<Ticket ticket={[2,5,7]}/>
-
+  <Lottery n={3} winCondition={winCondition}/>
  </>)
 }
+
 
 export default App;
--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -1,24 +1,23 @@
+
 import { useState } from "react"
 import "./Lottery.css"
+import Ticket from "./Ticket";
 import{genTicket,sum}from "./helper";
+import Button from "./Button";
 
-
-export default function Lottery(){
-
-    let [ticket,setTicket]=useState(genTicket(3));
-    let isWinning = sum(ticket)===15;
-    let BuyTicket=()=>{
-        setTicket(genTicket(3))
+export default function Lottery({n=3,winCondition}){
+ 
+    let [ticket,setTicket]=useState(genTicket(n));
+    let isWinning = winCondition(ticket);
+    let buyTicket=()=>{
+        setTicket(genTicket(n))
     }
     return (
     <div> 
     <h1> Lottery Game!</h1>
-    <div className=" ticket">
-        <span> {ticket[0]}</span>
-        <span> {ticket[1]}</span>
-        <span> {ticket[2]}</span>
-    </div>
-    <button onClick={BuyTicket}> Buy ticket </button>
+    <Ticket ticket={ticket}/>
+    <Button action={buyTicket}/>
+   
     <h3> { isWinning && "congratulations , you won!"}</h3>
         </div>
 )}
